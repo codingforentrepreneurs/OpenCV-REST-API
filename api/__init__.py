9590363c26e1432b3cf867c5f49fddbebe03812f
CFE <hello@teamcfe.com>
2020-01-23 11:35:13 -0800

    5 - Storage Directories

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,13 @@
+import os
 from flask import Flask
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_DIR = os.path.join(BASE_DIR, "storage", "upload")
+RESULTS_DIR = os.path.join(BASE_DIR, "storage", "results")
+OUTPUT_DIR = os.path.join(BASE_DIR, "storage", "output")
+
+for d in [UPLOAD_DIR, RESULTS_DIR, OUTPUT_DIR]:
+    os.makedirs(d, exist_ok=True)
 
 app = Flask(__name__)
 
