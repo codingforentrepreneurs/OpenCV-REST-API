c6496b8e1e15352bf2e785f9178deb1a2a92eb2f
CFE <hello@teamcfe.com>
2020-01-23 13:53:58 -0800

    16 - OpenCV Upload & Extract View

--- api/cv/utils.py
+++ api/cv/utils.py
@@ -13,14 +13,16 @@ def extract(img_path):
     cascade = get_cascade()
     filename, _ = os.path.splitext(os.path.basename(img_path))
     final_output_dir = os.path.join(OUTPUT_DIR, f"{filename}")
+    faces_output_dir = os.path.join(final_output_dir, "faces")
     os.makedirs(final_output_dir, exist_ok=True)
+    os.makedirs(faces_output_dir, exist_ok=True)
     frame = cv2.imread(img_path)
     og_frame = cv2.imread(img_path)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=3)
     for i, (x, y, w, h) in enumerate(faces):
         roi_color = frame[y:y+h, x:x+w] # y: y+height, x: x+width
-        path = os.path.join(final_output_dir, f"{i}.jpg")
+        path = os.path.join(faces_output_dir, f"{i}.jpg")
         color = (255, 0, 0) # BGR
         cv2.rectangle(og_frame, (x, y), (x+w, y+h), color, 2)
         cv2.imwrite(path, roi_color)
