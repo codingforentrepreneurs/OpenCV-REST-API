306968d873525372e625e91a52451fdc959a7c4b
CFE <hello@teamcfe.com>
2020-01-23 11:40:04 -0800

    6 - Serve Static Files

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_DIR = os.path.join(BASE_DIR, "storage", "upload")
@@ -10,7 +10,12 @@ for d in [UPLOAD_DIR, RESULTS_DIR, OUTPUT_DIR]:
     os.makedirs(d, exist_ok=True)
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
 
 @app.route("/")
 def home_view():
     return "<h1>Hello world</h1>"
+
+@app.route("/static/uploads/<filename>")
+def static_uploads_view(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
