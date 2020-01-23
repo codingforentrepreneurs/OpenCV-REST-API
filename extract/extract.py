7497151a3f65a4376f6a29c513f01a9fe5bb43aa
CFE <hello@teamcfe.com>
2020-01-23 13:20:49 -0800

    13 - Loading HaarCascades

--- extract/extract.py
+++ extract/extract.py
@@ -6,7 +6,10 @@ img_path = os.path.join(BASE_DIR, 'got.jpg')
 
 frame = cv2.imread(img_path) # RGB Red Green Blue -> BGR Blue Green Red
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+cascade_path = os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml')
+cascade = cv2.CascadeClassifier(cascade_path)
+faces = cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+print(faces)
 cv2.imshow("frame", frame)
 cv2.imshow("gray", gray)
 cv2.waitKey(0) # press w to quit
