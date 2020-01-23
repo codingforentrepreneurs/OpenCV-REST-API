d6714fee46f38409ce8b447d9b2d09d293969c99
CFE <hello@teamcfe.com>
2020-01-23 11:46:33 -0800

    7 - Allowed File Extensions

--- api/__init__.py
+++ api/__init__.py
@@ -5,12 +5,18 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_DIR = os.path.join(BASE_DIR, "storage", "upload")
 RESULTS_DIR = os.path.join(BASE_DIR, "storage", "results")
 OUTPUT_DIR = os.path.join(BASE_DIR, "storage", "output")
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif']
 
 for d in [UPLOAD_DIR, RESULTS_DIR, OUTPUT_DIR]:
     os.makedirs(d, exist_ok=True)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
+
+def allowed_file(filename):
+    return "." in filename and \
+        filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/")
 def home_view():
