6d9624485e4ad8aa31d399ef6a9d47ce8670b89e
CFE <hello@teamcfe.com>
2020-01-24 12:56:35 -0800

    18 - Serve Faces Output as Live URL

--- api/cv/views.py
+++ api/cv/views.py
@@ -14,6 +14,13 @@ def static_outputs_view(path):
     print(dirname, filename)
     return send_from_directory(dirname, filename)
 
+
+def get_url_for_local_image(dirname, filename):
+    if dirname.startswith("/"):
+        dirname = dirname[1:]
+    final_path = os.path.join(dirname, filename)
+    return url_for('static_outputs_view', path=final_path, _external=True)
+
 @app.route("/api/upload/opencv", methods=['POST'])
 def api_upload_opencv():
     if request.method == "POST":
@@ -23,10 +30,14 @@ def api_upload_opencv():
         response, status = handle_file_upload(upload, dest_dir, dest_len=dest_len, return_img_path=True)
         if len(f"{status}") == 4:
             extracted_path = extract(response)
-            mainjpg = os.path.join(extracted_path, "main.jpg")
-            if mainjpg.startswith("/"):
-                mainjpg = mainjpg[1:] # mainjpg[0]
-            final_url = url_for('static_outputs_view', path=mainjpg, _external=True)
-            return {"path": final_url}, 201
+            final_url = get_url_for_local_image(extracted_path, "main.jpg")
+            # final_items = [get_url_for_local_image(extracted_path, x) for x in os.listdir(extracted_path) if x.endswith('.jpg')]
+            response = {"path": final_url}
+            faces_dir = os.path.join(extracted_path, 'faces')
+            faces_items = [get_url_for_local_image(faces_dir, x) for x in os.listdir(faces_dir) if x.endswith('.jpg')]
+            faces_count = len(faces_items)
+            response['faces_count'] = faces_count
+            response['faces'] = faces_items
+            return response, 201
         return response, status
     return {"detail": "Not allowed"}, 400
