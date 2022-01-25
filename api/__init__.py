32c2aada9acc116205052303c9fc3c2bb242f60d
CFE <hello@teamcfe.com>
2022-01-25 10:31:36 -0600

    Added default SERVER_NAME for Docker

--- api/__init__.py
+++ api/__init__.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
 app.config['OUTPUT_DIR'] = OUTPUT_DIR
 PORT = os.environ.get('PORT') or 8000
-app.config['SERVER_NAME'] = os.environ.get('SERVER_NAME') or f'127.0.0.1:{PORT}'
+app.config['SERVER_NAME'] = os.environ.get('SERVER_NAME') or f'0.0.0.0:{PORT}'
 
 def allowed_file(filename):
     return "." in filename and \
